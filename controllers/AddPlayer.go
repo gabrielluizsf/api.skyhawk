192bfd825896313aed5693c5dfbe5e5a72a5a342
Gabriel luiz <93672097+GabrielLuizSF@users.noreply.github.com>
2023-04-30 11:28:09 -0300

    using new package database

--- controllers/AddPlayer.go
+++ controllers/AddPlayer.go
@@ -5,13 +5,10 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
-	"os"
 
 	player "github.com/gabrielluizsf/api.skyhawk/Player"
-	"github.com/joho/godotenv"
+	"github.com/gabrielluizsf/api.skyhawk/database"
 	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func AddPlayer(w http.ResponseWriter, r *http.Request) {
@@ -21,39 +18,17 @@ func AddPlayer(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
-
-	if os.Getenv("APP_ENV") != "production" {
-		err := godotenv.Load()
-		if err != nil {
-			log.Fatal("Erro ao carregar variáveis de ambiente: ", err)
-		}
-	}
-	uri := os.Getenv("DB_URI")
-
-	client, err := mongo.NewClient(options.Client().ApplyURI(uri))
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	err = client.Connect(context.Background())
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	collection := client.Database("skyhawk").Collection("players")
-
 	playerDoc := bson.M{"username": player.Username, "points": player.Points}
 
-	_, err = collection.InsertOne(context.Background(), playerDoc)
+	conect, client := database.Connect()
+	_, err = conect.InsertOne(context.Background(), playerDoc)
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	err = client.Disconnect(context.Background())
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	w.WriteHeader(http.StatusCreated)
 	w.Write([]byte("Jogador adicionado com sucesso!"))
 }
