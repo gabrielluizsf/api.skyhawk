804d3037cea958f11a970ec062981193fb385bb7
Gabriel luiz <93672097+GabrielLuizSF@users.noreply.github.com>
2023-05-01 15:48:54 -0300

    Added code that prevents creating an existing username

--- controllers/AddPlayer.go
+++ controllers/AddPlayer.go
@@ -11,6 +11,7 @@ import (
 )
 
 func AddPlayer(w http.ResponseWriter, r *http.Request) {
+	var existingPlayer player.Configure
 	var player player.Configure
 	err := json.NewDecoder(r.Body).Decode(&player)
 	if err != nil {
@@ -20,6 +21,17 @@ func AddPlayer(w http.ResponseWriter, r *http.Request) {
 	playerDoc := bson.M{"username": player.Username, "points": player.Points}
 
 	conect, client := database.Connect()
+
+	// Verifica se já existe algum jogador com o mesmo username
+	existingPlayerDoc := bson.M{"username": player.Username}
+
+	err = conect.FindOne(context.Background(), existingPlayerDoc).Decode(&existingPlayer)
+	if err == nil {
+		Log("Um usuário tentou se cadastrar com um username já existente", r)
+		http.Error(w, "REQUEST ERROR", http.StatusBadRequest)
+		return
+	}
+
 	_, err = conect.InsertOne(context.Background(), playerDoc)
 	logERROR(err)
 
